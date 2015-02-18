0845c24713d6f1857a4433b0e147ff92dda9824b
Ruairí Kenny <ruairikenny@gmail.com>
2015-02-18 23:21:02 +0000

    Added basic map

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,5 +1,18 @@
 (function(){
-var app = angular.module('divesites-app', []);
+	var app = angular.module('divesites-app', ['uiGmapgoogle-maps']).config(function(uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+        //    key: 'your api key',
+        v: '3.17',
+        libraries: 'weather,geometry,visualization'
+    });
+})
 
+    app.controller('testController', function(uiGmapGoogleMapApi){
+    	this.map = { center: { latitude: 53.5, longitude: -8 }, zoom: 7 };
+    	this.divesites = [{'name':'UC-42', 'chart_depth': 28},{'name':'U260', 'chart_depth': 34}];
+    	uiGmapGoogleMapApi.then(function(maps) {
+
+    	});
+    });
 
 })();
