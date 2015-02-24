a53c0daeb5f4492259915ab73edcd60909b2d09b
Ruairí Kenny <ruairikenny@gmail.com>
2015-02-24 19:05:37 +0000

    Resolving conflicts from pull requests

---
+++ public/scripts/directives/sidebar.js
@@ -0,0 +1,7 @@
+angular.module('divesitesApp').directive('sidebar', function () {
+  return {
+    templateUrl: 'views/partials/sidebar.html',
+    restrict: 'E',
+    controller: 'SidebarController'
+  };
+});
