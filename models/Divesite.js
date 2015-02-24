48c9012bf0797f31fb6cda26599274348f2669a7
Séamus Ó Buadhacháin <sdob@cs.ucc.ie>
2015-02-24 14:44:31 +0000

    Site coords are now stored as GeoJSON objects

--- models/Divesite.js
+++ models/Divesite.js
@@ -2,10 +2,8 @@ var mongoose = require('mongoose');
 
 var DivesiteSchema = new mongoose.Schema({
 	name: String,
-	lat: String,
-	lon: String,
+  loc: {loc: {type: [Number], index: '2dsphere'}},
 	chart_depth: {type: Number, min: 0, max: 200},
-	type: String,
 	updated_at: { type: Date, default: Date.now },
 });
 
