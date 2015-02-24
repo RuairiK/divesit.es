0a15212996a15bc84d651d2308c6818faca2cfd4
Séamus Ó Buadhacháin <sdob@cs.ucc.ie>
2015-02-24 18:04:18 +0000

    Controllers and directives for top nav and sidebar menus

---
+++ public/scripts/controllers/nav-top.js
@@ -0,0 +1,6 @@
+var app = angular.module('divesitesApp');
+
+app.controller('NavTopController',
+function (uiGmapGoogleMapApi, $http, $scope) {
+    console.log('initializing top navigation controller');
+  });
---
+++ public/scripts/controllers/sidebar.js
@@ -0,0 +1,6 @@
+var app = angular.module('divesitesApp');
+
+app.controller('SidebarController',
+function (uiGmapGoogleMapApi, $http, $scope) {
+    console.log('initializing sidebar controller');
+  });
