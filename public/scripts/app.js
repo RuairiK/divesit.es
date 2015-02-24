768bbf888cf65b23897c3cc88294e5982d8c6cd3
Séamus Ó Buadhacháin <sdob@cs.ucc.ie>
2015-02-24 18:21:44 +0000

    Reformatting

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,10 @@
 (function(){
-  var app = angular.module('divesitesApp', ['uiGmapgoogle-maps']).config(function(uiGmapGoogleMapApiProvider) {
-    uiGmapGoogleMapApiProvider.configure({
-      //    key: 'your api key',
-      v: '3.17',
-      libraries: 'weather,geometry,visualization'
-    });
-  })
+  var app = angular.module('divesitesApp', ['uiGmapgoogle-maps']).config(
+    function(uiGmapGoogleMapApiProvider) {
+      uiGmapGoogleMapApiProvider.configure({
+        //    key: 'your api key',
+        v: '3.17',
+        libraries: 'weather,geometry,visualization'
+      });
+    })
 })();
