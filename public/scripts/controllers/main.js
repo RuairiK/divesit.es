a7e15385430febf9779dcc5189e6af3710d891b0
sdob <sdob@cs.ucc.ie>
2015-02-27 13:27:49 +0000

    Stub MainController in case we need something more sophisticated than '.' to communicate between siblings

---
+++ public/scripts/controllers/main.js
@@ -0,0 +1,8 @@
+angular.module('divesitesApp').controller('MainController',
+  function($scope) {
+    // This is a dummy main controller in case we need it.
+    // Using $rootScope allows us to broadcast events to siblings
+    // but we might eventually want to be more sophisticated than
+    // that.
+  }
+);
