48cbc6e8e1e5d9cdd9c5a8a0bcc88a767a025a23
sdob <sdob@cs.ucc.ie>
2015-02-27 13:29:38 +0000

    Filter dive site by category; store these preferences as a cookie; broadcast an event when preferences change

--- public/scripts/controllers/sidebar.js
+++ public/scripts/controllers/sidebar.js
@@ -1,6 +1,30 @@
 var app = angular.module('divesitesApp');
 
 app.controller('SidebarController',
-function (uiGmapGoogleMapApi, $http, $scope) {
+  function (uiGmapGoogleMapApi, $http, $scope, $rootScope, $cookieStore) {
+
+    // Initialize controller
     console.log('initializing sidebar controller');
+    // Retrieve stored filter preferences if they are in the cookie
+    $scope.preferences = $cookieStore.get('filterPreferences') || {
+      wreck: true,
+      scenic: true,
+      drift: true
+    };
+    // Store preferences in the cookieStore if they didn't exist already
+    $cookieStore.put('filterPreferences', $scope.preferences);
+
+    // String constants for site categories
+    $scope.WRECK = 'wreck';
+    $scope.SCENIC = 'scenic';
+    $scope.DRIFT = 'drift';
+
+    $scope.filterSites = function (category, show) {
+      // TODO: pass category (string) and show (boolean) to map
+      // to switch visibility on/off
+      console.log('sending filter-sites event');
+      // Update preferences
+      $cookieStore.put('filterPreferences', $scope.preferences);
+      $rootScope.$broadcast('event:filter-sites', {category: category, show: show});
+    }
   });
