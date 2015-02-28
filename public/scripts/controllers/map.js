08db4b253b08efe37551fcbb92d4ec143fdb3c60
sdob <sdob@cs.ucc.ie>
2015-02-28 15:02:41 +0000

    Support for filtering on site category, plus distinct icons

--- public/scripts/controllers/map.js
+++ public/scripts/controllers/map.js
@@ -1,6 +1,7 @@
 var app = angular.module('divesitesApp');
 
-app.controller('MapController', function(uiGmapGoogleMapApi, $http, $scope, $cookieStore, $modal) {
+app.controller('MapController',
+  function(uiGmapGoogleMapApi, uiGmapIsReady, $http, $scope, $rootScope, $cookieStore, $modal, $timeout) {
 
     // Initialize controller
     console.log('initializing map controller');
@@ -13,12 +14,26 @@ app.controller('MapController', function(uiGmapGoogleMapApi, $http, $scope, $coo
       zoom: ($cookieStore.get('map.zoom') || 7)
     };
 
+    // Needs to be non-null at initialization (for some obscure reason
+    // mentioned but not explained by angular-google-maps)
+    $scope.map.markers = [];
+
+    // Object to receive marker functionality
+    $scope.mapControl = {};
+    $scope.markerControl = {};
+
+
+    // Map options
     $scope.map.options = {
       scrollwheel: true,
       disableDefaultUI: true,
       mapTypeId: 'satellite'
 
     };
+
+    // Map marker options
+    $scope.map.markerOptions = { }
+
     // stubs for future event handling
     $scope.map.events = {
       tilesloaded: function (map) {
@@ -49,11 +64,12 @@ app.controller('MapController', function(uiGmapGoogleMapApi, $http, $scope, $coo
           });
       }
     };
+
     // Map marker events 
     $scope.map.markerEvents = {
+      // On click, retrieve this site's record from the sites API and do something
+      // exciting with it
       click: function (marker, event, model, args) {
-        // Retrieve this model's record from the sites API and do something
-        // exciting with it
         $http.get('/divesites/' + model.id).success(function (data) {
             // For now, just log to the console to prove that things are
             // working.
@@ -78,33 +94,56 @@ app.controller('MapController', function(uiGmapGoogleMapApi, $http, $scope, $coo
       }
     };
 
-    // Call the API for dive sites (currently returns everything)
-    $http.get('/divesites/').success(function (data) {
-        // On success, update the markers
-        $scope.map.markers = data.map(function (e) {
-            return {
-              id: e._id, 
-              location: {
-                // The order of latitude and longitude is specified by GeoJSON.
-                // It's the opposite way around from how GPS coordinates are normally
-                // presented, so we need to be aware of this going forward.
-                longitude: e.loc.coordinates[0],
-                latitude: e.loc.coordinates[1]
-              },
-              title: e.name, // the site name
-              chart_depth: e.chart_depth // chart depth (at lowest astronomical tide)
-            };
-          });
-      });
-
-
-    $scope.$on('event:filter-sites', function (data) {
-        console.log('checkbox event');
+    $scope.$on('event:filter-sites', function (event, data) {
+        console.log('received event:filter-sites');
+        //console.log($scope.markerControl.getPlurals());
+        $scope.map.markers.forEach(function (m) {
+            if (m.category == data.category) {
+              m.options.visible = data.show;
+            }
+          }
+        );
       }
     );
 
     uiGmapGoogleMapApi.then(function(maps) {
-        // What are we doing here? 
-      });
-  }
-);
+      }
+    );
+
+    uiGmapIsReady.promise().then(
+      // Fires when the map is loaded. Once the map is loaded, go get the
+      // dive site data from the API. This means that 'event:map-isready'
+      // is only called when we have *everything* we need.
+      function (maps) {
+        $scope.mapInstance = maps[0].map;
+        // Call the API for dive sites (currently returns everything)
+        $http.get('/divesites/').success(function (data) {
+            // On success, update the markers
+            $scope.map.markers = data.map(function (e) {
+                return {
+                  id: e._id, 
+                  location: {
+                    // The order of latitude and longitude is specified by GeoJSON.
+                    // It's the opposite way around from how GPS coordinates are normally
+                    // presented, so we need to be aware of this going forward.
+                    longitude: e.loc.coordinates[0],
+                    latitude: e.loc.coordinates[1]
+                  },
+                  title: e.name, // the site name
+                  chart_depth: e.chart_depth, // chart depth (at lowest astronomical tide)
+                  options: { // Google Maps MarkerOptions
+                    visible: false // initially false, switched on when we know preferences
+                  },
+                  category: e.category, // site category ('wreck', 'scenic', etc.)
+                  icon: '/img/icons/' + e.category + '.png'
+                };
+              }
+            );
+          }
+        ).then(function () {
+            $rootScope.$broadcast('event:map-isready');
+          }
+        );
+      }
+    );
+  });
