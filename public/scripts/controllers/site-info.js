d39b443c6784d21dcf614a75ec93611791870768
Ruairí Kenny <ruairikenny@gmail.com>
2015-03-17 19:10:48 +0000

    Removed unused injected dependencies

--- public/scripts/controllers/site-info.js
+++ public/scripts/controllers/site-info.js
@@ -1,6 +1,6 @@
 var app = angular.module('divesitesApp');
 
-app.controller('SiteInfoController', function(uiGmapGoogleMapApi, $http, $scope, $rootScope, $cookieStore, $modal) {
+app.controller('SiteInfoController', function($scope) {
   console.log('initializing site info controller');
   $scope.site = $scope.siteInfo;
 });
