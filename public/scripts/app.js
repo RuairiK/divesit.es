661cabd487afb460c17c7ab6745278aece018d3b
sdob <sdob@cs.ucc.ie>
2015-04-14 16:33:13 +0000

    Basic Facebook OAuth login/logout

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,24 +1,25 @@
 (function(){
-    'use strict';
+  'use strict';
 
-    var app = angular.module('divesitesApp',
-      [
-        'uiGmapgoogle-maps', 'ngCookies', 'ui.bootstrap', 'ngRoute'
-      ]
-    ).config(
-      function(uiGmapGoogleMapApiProvider, $routeProvider) {
-        uiGmapGoogleMapApiProvider.configure({
-            //    key: 'your api key',
-            v: '3.17',
-            libraries: 'weather,geometry,visualization'
-          }
-        );
-        $routeProvider.when('/', {
-            templateUrl: 'views/map.html'
-            //controller: 'MapController'
-          }
-        );
-      }
-    );
-  }
-)();
+  var app = angular.module('divesitesApp', [
+    'uiGmapgoogle-maps', 'ngCookies', 'ui.bootstrap', 'ngRoute', 'satellizer'
+  ])
+  .config(function(uiGmapGoogleMapApiProvider, $routeProvider, $authProvider) {
+
+
+    $authProvider.google({
+      clientId: "930190391486-6pj424i3mmmvptic21rdvm2f0e9il5fl.apps.googleusercontent.com"
+    });
+
+    uiGmapGoogleMapApiProvider.configure({
+      //    key: 'your api key',
+      v: '3.17',
+      libraries: 'weather,geometry,visualization'
+    });
+
+    $routeProvider.when('/', {
+      templateUrl: 'views/map.html'
+      //controller: 'MapController'
+    });
+  });
+})();
