d9e87d4e822c0742c411153b72c197fa338be0c2
Séamus Ó Buadhacháin <sdob@cs.ucc.ie>
2015-04-14 23:01:35 +0100

    Removed requirement for passport.js

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var passport = require('passport');
 var keys = require('./keys'); 
 
 var mongodbConnString = "mongodb://"
