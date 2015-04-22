71b88828812b9df12de444c9dc424029bdf39f2f
sdob <sdob@cs.ucc.ie>
2015-04-22 21:50:35 +0000

    move force-authenticate to request headers

--- middleware/test-auth.js
+++ middleware/test-auth.js
@@ -1,29 +1,29 @@
 var User = require('../models/User');
 
 var ensureAuthenticated = function (req, res, next) {
-    // if we pass {'forceAuthenticate': true, "displayName": $NAME} in the body of the request,
-    // we can override authentication checks and forcibly override as $NAME
-    
-    //console.log("ensureAuthenticated: request body:");
-    //console.log(req.body);
-    if (req.body.forceAuthenticate) {
-        User.findOne({displayName: req.body.displayName}, function (err, user) {
-            if (err) { return next(err); } 
-            req.user = user._id;
-            next();
-        });
-    } else {
-        if (!req.headers.authorization) {
-            return res.status(401).send({message: 'No Authorization token'});
-        }
-        var token = req.headers.authorization.split(' ')[1];
-        var payload = jwt.decode(token, keys.tokenSecret);
-        if (payload.exp <= moment().unix()) {
-            return res.status(401).send({message: 'Token has expired'});
-        }
-        req.user = payload.sub;
-        next();
+  // if we pass {'forceAuthenticate': true, "id": $_id} in the request headers
+  // we can override authentication checks and forcibly override as $NAME
+
+  if (req.headers['force-authenticate'] && req.headers['auth-id']) {
+    User.findOne({_id: req.headers['auth-id']}, function (err, user) {
+      if (err) { return next(err); } 
+      // Casting this to a String seems to fix a weird bug that causing
+      // req.user != user._id
+      req.user = "" + user._id;
+      next();
+    });
+  } else {
+    if (!req.headers.authorization) {
+      return res.status(401).send({message: 'No Authorization token'});
     }
+    var token = req.headers.authorization.split(' ')[1];
+    var payload = jwt.decode(token, keys.tokenSecret);
+    if (payload.exp <= moment().unix()) {
+      return res.status(401).send({message: 'Token has expired'});
+    }
+    req.user = payload.sub;
+    next();
+  }
 }
 
 // TODO: flesh these out for further testing
