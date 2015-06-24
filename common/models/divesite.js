3b5f83e32a13fb11b7e78ecf50d64356557fe28a
Séamus Ó Buadhacháin <sdob@cs.ucc.ie>
2015-06-24 19:10:03 +0100

    authentication stuff

--- common/models/divesite.js
+++ common/models/divesite.js
@@ -1,10 +1,12 @@
 module.exports = function(Divesite) {
+
   // Set the creating user as the creator of this divesite
   Divesite.beforeRemote('create', function (context, user, next) {
     var req = context.req;
     req.body.userId = req.accessToken.userId;
     next();
   });
+
   // Only update allowed fields
   Divesite.beforeRemote('prototype.updateAttributes', function (context, user, next) {
     // These are the names of the allowed fields; everything else gets deleted
--- server/server.js
+++ server/server.js
@@ -1,7 +1,24 @@
 var loopback = require('loopback');
 var boot = require('loopback-boot');
-
 var app = module.exports = loopback();
+
+// Logger
+var logger = require('morgan');
+app.use(logger('dev'));
+// Passport config
+var loopbackPassport = require('loopback-component-passport');
+var PassportConfigurator = loopbackPassport.PassportConfigurator;
+var passportConfigurator = new PassportConfigurator(app);
+
+// Load provider configurations
+var config = {};
+try {
+  config = require('./providers.json');
+} catch (err) {
+  console.error('Error loading providers.json');
+  process.exit(1);
+}
+
 
 app.start = function() {
   // start the web server
@@ -20,3 +37,18 @@ boot(app, __dirname, function(err) {
   if (require.main === module)
     app.start();
 });
+
+// Initialize Passport
+passportConfigurator.init();
+//passportConfigurator.setupModels();
+passportConfigurator.setupModels({
+  userModel: app.models.User,
+  userIdentityModel: app.models.UserIdentity,
+  userCredentialModel: app.models.UserCredential
+});
+
+for (var s in config) {
+  var c = config[s];
+  c.session = c.session !== false;
+  passportConfigurator.configureProvider(s, c);
+}
