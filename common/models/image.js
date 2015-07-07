5544c687aa0c5ad779ed7f288f8760cfd713f3b5
Séamus Ó Buadhacháin <sdob@cs.ucc.ie>
2015-07-07 12:08:23 +0100

    Subclass Image to DivesiteImage

--- common/models/image.js
+++ common/models/image.js
@@ -1,15 +1,7 @@
 module.exports = function(Image) {
 
-  Image.validatesPresenceOf('divesiteId', 'userId');
+  Image.validatesPresenceOf('userId');
 
-  Image.validateAsync('divesiteId', function (err, done) {
-    // Check that there is a site with this ID
-    var Divesite = Image.app.models.Divesite;
-    Divesite.findById(this.divesiteId, function (e, site) {
-      if (!site) err();
-      done();
-    });
-  });
 
   Image.beforeRemote('create', function (context, user, next) {
     // Before saving, set the image's owner to the user sending the request
